a96dd5badd5fc93fd361b6609b54d3b284774073
Akanksha Verma <47955870+akv3001@users.noreply.github.com>
2019-08-08 09:54:03 -0400

    Create Aggregate_AgeDistrbution.R
    Age distribution summary code to be added
    comments added for guidance

---
+++ Aggregate_AgeDistrbution.R
@@ -0,0 +1,25 @@
+##########################
+#Task: Go through all the clinical files and estimate the age distribution
+# Notes/Updates
+
+
+##########################
+
+
+# Write a loop to iterate through the clinical files 
+# Create a list object with each cancer type as adataframe object
+
+
+# Compute the median and mean for each cancer type , the mix , max 
+# also use a loop to do this by going through the list structure
+# this should create a new dataframe summarized ( you did this manually in the excel originally but please execute same in code below)
+
+
+
+# Append to dataframe above the median_younger/median_older columns 
+#  this would be samples < median age , sample > median age
+# same this for the mean_younger, mean_older
+
+
+
+#you should end up with 2 object here a list object for the clinical table and dataframe summary to determine the group distributions
