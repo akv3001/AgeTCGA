c3cbffb73fded3b43ea83ae96b1c3dfd5f9be9b3
Womy212 <womy212@gmail.com>
2019-08-15 19:31:21 -0400

    Updated TEST folder
    Added all clinical_data files.
    Removed older basic scripts.
    
    ** Added new script - to be continued.

--- DATA/cBioportal_test/NEW.R
+++
@@ -1,7 +0,0 @@
-setwd("/Users/romypichardo/Downloads/cBioportal_test") ## set directory
-
-file = list.files(pattern="\\.txt$") ## assign all files to variable file 
-
-list_clinical = list() ## create emapty list with list()
-
-list_clinical[["Cancer Type"]] = input_read[,c("AGE")]
--- R_Scripts/Aggregate_AgeDistrbution.R
+++ R_Scripts/Aggregate_AgeDistrbution.R
@@ -1,8 +1,6 @@
 ##########################
 #Task: Go through all the clinical files and estimate the age distribution
 # Notes/Updates
-
-
 ##########################
 
 
@@ -13,29 +11,44 @@ path = "DATA/cBioportal_test"  ## set file path
 
 file.names <- list.files(path, pattern =".txt",full.names = TRUE) ##assign name to path (looking for txt files withi directory)
 
+All_CancerData <- list()    ##open list
 
-## for object(i) in 1 through length of file.names
-
-for (i in 1:length(file.names)) ## for object(i) in 1 through length of file.names
+for (i in 1:length(file.names)) ## for object(i) in 1 through length of file.names. 
+{  
   
-{
-  
-  print(file.names[[i]])
+  clin_data_df <- read.delim(file.names[[i]], stringsAsFactors = F)  ##create dataframe of cBio files for all cancer files
   
   
+  for ( type in 1:length(unique(clin_data_df$CANCER_TYPE))){  ##loop through the number cancer type
+    
+    CancerType <- (clin_data_df$CANCER_TYPE)[type]   ##save cancer typeto var cancerType 
+    SelectCases <- na.omit(clin_data_df$AGE[which(clin_data_df$CANCER_TYPE == CancerType)]) ## extracting age for the cancerType
+    
+    
+    # Compute the median and mean for each cancer type , the mix , max 
+    # also use a loop to do this by going through the list structure
+    # this should create a new dataframe summarized ( you did this manually in the excel originally but please execute same in code below)
+    
+    
+    ##calculating range/quantile/median/mean/max/min/greater > median/ less than <median 
+    CancerData <- c(range=range(SelectCases), 
+                    median=median(SelectCases),
+                    mean=mean(SelectCases), min = SelectCases[sapply(which.min(SelectCases),min)],
+                    max=SelectCases[sapply(which.max(SelectCases),max)],
+                    OlderMedian=sum(SelectCases>median(SelectCases)),
+                    YoungerMedian=sum(SelectCases<median(SelectCases)),
+                    quantile=quantile(SelectCases))
+    
+    All_CancerData[[CancerType]] <- CancerData ## save to new list 
+    
+  }
 }
 
-
-# Compute the median and mean for each cancer type , the mix , max 
-# also use a loop to do this by going through the list structure
-# this should create a new dataframe summarized ( you did this manually in the excel originally but please execute same in code below)
-
-
+print(All_CancerData) ##print list 
 
 # Append to dataframe above the median_younger/median_older columns 
 #  this would be samples < median age , sample > median age
 # same this for the mean_younger, mean_older
-
 
 
 #you should end up with 2 object here a list object for the clinical table and dataframe summary to determine the group distributions
